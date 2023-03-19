8cc1fd7f54abc256c1e502ff69a1022ed30bbee4
Vishal <lokarevishal5767@gmail.com>
2023-03-19 17:56:35 +0530

    initial commit

---
+++ src/api/order/routes/custom-router.js
@@ -0,0 +1,11 @@
+
+module.exports ={
+    routes: [
+      {
+        method: 'GET',
+        path: '/orders/customOrder',
+        handler: 'order.customOrderController',
+    
+      },
+    ],
+  };
